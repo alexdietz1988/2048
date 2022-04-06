f165af5279afe30aef58ea2474925fe4bffebcba
alexdietz1988 <alex.dietz@gmail.com>
2022-04-06 17:16:57 -0400

    Debugging moveLeft

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function randomOpenIndex() {
 // Use an index on the gameboard array to position an element
 function positionPiece(piece, row, column) {
     let top = row * 100;
-    let left = column * 100
+    let left = column * 100;
     $(piece).css( {'top': `${top}px`, 'left': `${left}px`})
 }
 
@@ -48,7 +48,7 @@ function newPiece() {
 }
 
 // Call newPiece() twice to generate the first two pieces
-// newPiece(); newPiece()
+newPiece(); newPiece()
 
 // ### Moving pieces
 // 1. I'll create a function to move pieces left. This will take the leftmost element in each row and start looking to its left until it finds (a) the end of the row, (b) an element of a different value, or (c) an element of the same value. It will give the index that the element should be "moved" to, and will call the function in (4) above using that index to move the actual piece, and will also update the gameboard array appropriately. The function will then do the same thing with the next-to-leftmost element in each row, and so on. Finally, it will call the functions to randomly generate a new piece.
@@ -56,18 +56,21 @@ function newPiece() {
 // Find piece at position
 
 // TEST PIECE
-let $newPiece = $('<div class="square piece">2</div>')
-let position = [...randomOpenIndex()]
-$('.gameboard').prepend($newPiece)
-positionPiece($newPiece, 0,0)
-gameArray[position[0]][position[1]] = 2
+// let $newPiece = $('<div class="square piece">2</div>')
+// let position = [2,2]
+// $('.gameboard').prepend($newPiece)
+// positionPiece($newPiece, 2,2)
+// gameArray[position[0]][position[1]] = 2
+
+// let testPiece = pieceAtPosition(2,2)
+// console.log(testPiece)
 
 function pieceAtPosition(row, column) {
     let pieces = document.querySelectorAll('.piece')
     let mysteryPiece;
 
     pieces.forEach( (piece, idx) => {
-        if (pieces[idx].style.top === `${row}px` && pieces[idx].style.left === `${column}px`) {
+        if (pieces[idx].style.top === `${row * 100}px` && pieces[idx].style.left === `${column * 100}px`) {
             mysteryPiece = pieces[idx]
             }
     }
@@ -75,23 +78,25 @@ function pieceAtPosition(row, column) {
     return mysteryPiece
 }
 
-let pieceToMove = pieceAtPosition(0,0)
-console.log(pieceToMove)
-
 function moveLeft() {
+
+    let pieceToMove;
 
     for (let i = 0; i < 4; i++) { // Look at each row
         for (let j = 1; j < 4; j++) { // Look at the latter three columns
+
             if (gameArray[i][j] > 0) { // Find the leftmost piece
+                pieceToMove = pieceAtPosition(i, j)
+                console.log(pieceToMove.style.top)
+                console.log(pieceToMove.style.left)
+
                 let lookingForWhereToGo = true;
                 let k = 1;
 
                 while (lookingForWhereToGo) {
                     if (gameArray[i][j - k] === 0) k++ // If the square is empty, look to its left
                     else if (gameArray[i][j - k] === 0 === false) { // Once you find a non-empty square or the end of the board...
-                        let pieceToMove = pieceAtPosition(i, j)
-                        console.log(pieceToMove)
-                            
+                        
                         if (gameArray[i][j - k] === gameArray[i][j]) {
                             positionPiece(pieceToMove, i, j - k) // Move to that position if same value
                             console.log(`New position: [${i}][${j - k}]`) 
@@ -101,18 +106,9 @@ function moveLeft() {
                             console.log(`New position: [${i}][${j - k + 1}]`) // Move to next position if different value
                             lookingForWhereToGo = false
                         } 
-                    }
-                }
-
-            }
-        }
-    }
-
-}
+}}}}}}
 
 moveLeft()
-
-
 
 // 2. In my HTML and CSS, I'll create a button to move pieces left. In my JavaScript, I'll grab it and give it an event listener so that clicking it will call the "move left" function.
 // 3. For the case where a piece moves to the square held by a piece of the same value, I'll create a function to remove both pieces and replace them with a piece of the appropriate value.
