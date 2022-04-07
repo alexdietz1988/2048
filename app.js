7ef1708e3aa7a7e88c2dfba3848ca34b486028b4
alexdietz1988 <alex.dietz@gmail.com>
2022-04-07 08:35:40 -0400

    Debug combinePiecesAt

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function newPiece() {
 }
 
 // Call newPiece() twice to generate the first two pieces
-newPiece(); newPiece()
+// newPiece(); newPiece()
 
 // ### Moving pieces
 // 1. I'll create a function to move pieces left. This will take the leftmost element in each row and start looking to its left until it finds (a) the end of the row, (b) an element of a different value, or (c) an element of the same value. It will give the index that the element should be "moved" to, and will call the function in (4) above using that index to move the actual piece, and will also update the gameboard array appropriately. The function will then do the same thing with the next-to-leftmost element in each row, and so on. Finally, it will call the functions to randomly generate a new piece.
@@ -73,7 +73,7 @@ function moveLeft() {
         for (let c = 1; c < 4; c++) { // Look at the latter three columns
 
             if (gameArray[r][c] > 0) { // Find the leftmost piece
-                let pieceToMove = pieceAtPosition(r, c)
+                let pieceToMove = pieceAtPosition(r, c)[0]
                 let lookingForWhereToGo = true;
                 let delta = 1;
 
@@ -83,27 +83,35 @@ function moveLeft() {
                         
                         if (gameArray[r][c - delta] === gameArray[r][c]) {
                             positionPiece(pieceToMove, r, c - delta) // Move to that position if same value
-                            gameArray[r][c - delta] += Number(pieceToMove.textContent)
                             combinePiecesAt(r, c - delta)
                             
                         } else {
                             positionPiece(pieceToMove, r, c - delta + 1) // Move to next position if different value
-                            gameArray[r][c - delta + 1] += Number(pieceToMove.textContent)
+                            gameArray[r][c - delta + 1] = Number(pieceToMove.textContent)
                         }
                     gameArray[r][c] = 0
                     lookingForWhereToGo = false
+                    console.table(gameArray)
 }}}}}}
 
 // TEST PIECE
-// let $testPiece = $('<div class="square piece">2</div>')
-// $('.gameboard').prepend($testPiece)
-// positionPiece($testPiece, 2,2)
-// gameArray[2,2] = 2
+let $testPiece = $('<div class="square piece">2</div>')
+$('.gameboard').prepend($testPiece)
+positionPiece($testPiece, 1,1)
+gameArray[1][1] = 2
+
+let $testPiece2 = $('<div class="square piece">2</div>')
+$('.gameboard').prepend($testPiece2)
+positionPiece($testPiece2, 1,3)
+gameArray[1][3] = 2
+
+console.table(gameArray)
 
 function combinePiecesAt(row, column) {
     let pieces = pieceAtPosition(row, column)
     pieces[1].remove() // Remove one of the pieces
     pieces[0].textContent = (2 * Number(pieces[0].textContent)) // Double value of remaining piece
+    gameArray[row][column] = Number(pieces[0].textContent) // 
 }
 
 // 2. In my HTML and CSS, I'll create a button to move pieces left. In my JavaScript, I'll grab it and give it an event listener so that clicking it will call the "move left" function.
