fe8d1e34b2a0c15e4d89342bc0871f5b87dd282f
alexdietz1988 <alex.dietz@gmail.com>
2022-04-11 16:11:40 -0400

    Debug checkForLoss and randomOpenIndex; remove gameActive variable; reorganize code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // GLOBAL VARIABLES
 let lookingForWhereToGo;
-let gameActive = true;
 let initialGame = true;
 let successfulMove = false;
 let noWinFound = true;
@@ -8,28 +7,77 @@ let wins = 0;
 let losses = 0;
 let currentScore = 0;
 let highScore = 0;
-
-// Array representing the values at each square
-let emptyGameArray = [
+let gameArray = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
 ]
 
-let gameArray = emptyGameArray;
-
+// EVENT LISTENERS
 document.addEventListener('keydown', (e) => {
-    if (gameActive === false && noWinFound === false) keepPlaying()
-
     if (e.code === 'ArrowLeft') moveLeft()
     else if (e.code === 'ArrowRight') moveRight()
     else if (e.code === 'ArrowUp') moveUp()
     else if (e.code === 'ArrowDown') moveDown()
     endMove()
 })
+$('.reset').click(reset)
+$('.simulateEndgame').click(simulateEndgame)
 
 // GENERAL-PURPOSE FUNCTIONS
+// Find a random open index on gameArray
+function randomOpenIndex() {
+    // Make sure there's at least one space available before starting while loops
+    let spacesAvailable = false;
+    for (let r = 0; r < gameArray.length; r++) {
+        for (let c = 0; c < gameArray[r].length; c++) {
+            if (gameArray[r][c] === 0) spacesAvailable = true
+        }
+    }
+
+    if (spacesAvailable) {
+        let row;
+        let column;
+        let lookingForOpenRow = true;
+        let lookingForOpenColumn = true;
+
+        // Find a random row that contains a 0
+        while (lookingForOpenRow) {
+            row = Math.floor(Math.random() * 4); // Generate a random number between 0 and 3 (inclusive)
+            if (gameArray[row].some(element => element === 0)) lookingForOpenRow = false
+        }
+
+        // Find a random element in that row equal to 0
+        while (lookingForOpenColumn) {
+            column = Math.floor(Math.random() * 4); // Generate a random number between 0 and 3 (inclusive)
+            if (gameArray[row][column] === 0) lookingForOpenColumn = false
+        }
+
+        return [row, column];
+    
+    } else console.log('No open spaces')
+}
+
+// Generate a new piece at a random open square
+function newPiece() {
+    let value;
+    if (Math.random() < 0.2) value = 4
+    else value = 2
+    let position = randomOpenIndex()
+    let row = position[0]
+    let column = position[1]
+
+    let $newPiece = $(`<div class="piece v${value} r${row}c${column} combinable">${value}</div>`)
+        .css({'top': `${row * 100}px`, 'left': `${column * 100}px`, 'display': 'none'})
+    gameArray[row][column] = value
+
+    $('.gameboard').prepend($newPiece)
+    $newPiece.fadeIn(500, function() {
+        // FadeIn complete
+    })
+}
+
 function move(pieceToMove, oldRow, oldColumn, newRow, newColumn) {
     lookingForWhereToGo = false;
     successfulMove = true;
@@ -59,12 +107,14 @@ function combine(pieceToMove, oldRow, oldColumn, newRow, newColumn) {
 function endMove() {
     if (initialGame) checkForWin()
     $('.piece').addClass('combinable')
-    if (gameActive && successfulMove){
+    if (successfulMove){
         newPiece()
         checkForLoss()
     }
     successfulMove = false
 }
+
+newPiece(); newPiece() // Call newPiece() twice to generate the first two pieces
 
 // FUNCTIONS TO MOVE IN EACH DIRECTION
 function moveLeft() {
@@ -196,53 +246,70 @@ function moveDown() {
     }
 }
 
-// NEW PIECE
-// Find a random open index on gameArray
-function randomOpenIndex() {
-    let row;
-    let column;
-    let lookingForOpenRow = true;
-    let lookingForOpenColumn = true;
-
-    // Find a random row that contains a 0
-    while (lookingForOpenRow) {
-        row = Math.floor(Math.random() * 4); // Generate a random number between 0 and 3 (inclusive)
-        if (gameArray[row].some(element => element === 0)) lookingForOpenRow = false
-    }
-
-    // Find a random element in that row equal to 0
-    while (lookingForOpenColumn) {
-        column = Math.floor(Math.random() * 4); // Generate a random number between 0 and 3 (inclusive)
-        if (gameArray[row][column] === 0) lookingForOpenColumn = false
-    }
-
-    return [row, column];
+// ENDGAME
+function reset() {
+    $('.piece').remove()
+    gameArray = [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+    ]
+    initialGame = true
+    noWinFound = true
+    currentScore = 0
+    $('.message').html('Use the arrow keys to move the tiles,<br />combine them to get to 2048!')
+    $('.currentScore').text(`${currentScore}`)
+    newPiece(); newPiece();
 }
 
-// Generate a new piece at a random open square
-function newPiece() {
-    let value;
-    if (Math.random() < 0.2) value = 4
-    else value = 2
-    let position = randomOpenIndex()
-    let row = position[0]
-    let column = position[1]
-
-    let $newPiece = $(`<div class="piece v${value} r${row}c${column} combinable">${value}</div>`)
-        .css({'top': `${row * 100}px`, 'left': `${column * 100}px`, 'display': 'none'})
-    gameArray[row][column] = value
-
-    $('.gameboard').prepend($newPiece)
-    $newPiece.fadeIn(500, function() {
-        // FadeIn complete
-    })
+function checkForWin() {
+    for (let r = 0; r < 4 && noWinFound; r++) {
+        for (let c = 0; c < 4 && noWinFound; c++) {
+            if (gameArray[r][c] === 2048) {
+                youWin()
+                noWinFound = false;
+            }
+        }
+    }
 }
 
-// newPiece(); newPiece() // Call newPiece() twice to generate the first two pieces
+function checkForLoss() {
+    let lossPossible = true;
+    for (r = 0; r < 4 && lossPossible; r++) { // Look at each row
+        for (c = 0; c < 4 && lossPossible; c++) { // Look at each column
+            
+            if (gameArray[r][c] === 0) lossPossible = false; // No loss if any square is empty
+            
+            if (r > 0) { // No loss if there are matching pieces next to each other
+                if (gameArray[r][c] === gameArray[r - 1][c]) lossPossible = false
+            
+            }
+            
+            if (c > 0) { // No loss if there are matching pieces above/below each other
+                if (gameArray[r][c] === gameArray[r][c - 1]) lossPossible = false
+            }
+        }
+    }
+    if (lossPossible) youLose()
+}
+
+function youWin() {
+    $('.message').html('You win!<br /> Keep playing as long as you like!')
+    wins++
+    $('.winLossCount').text(`Wins: ${wins} | Losses: ${losses}`)
+    initialGame = false
+}
+
+function youLose() {
+    $('.message').text('You lose!')
+    losses++
+    $('.winLossCount').text(`Wins: ${wins} | Losses: ${losses}`)
+    $('.piece').css('opacity', '0.3')
+}
 
 function simulateEndgame() {
     $('.piece').remove();
-    gameActive = true
     initialGame = true
     noWinFound = true
     currentScore = 0
@@ -270,71 +337,4 @@ function simulateEndgame() {
             }
         }
     }
-}
-
-// ENDGAME
-$('.reset').on('click', reset)
-$('.simulateEndgame').on('click', simulateEndgame)
-
-function reset() {
-    $('.piece').remove()
-    gameArray = emptyGameArray
-    gameActive = true
-    initialGame = true
-    noWinFound = true
-    currentScore = 0
-    $('.message').html('Use the arrow keys to move the tiles,<br />combine them to get to 2048!')
-    $('.currentScore').text(`${currentScore}`)
-    newPiece(); newPiece();
-    console.table(gameArray)
-}
-
-function checkForWin() {
-    for (let r = 0; r < 4 && noWinFound; r++) {
-        for (let c = 0; c < 4 && noWinFound; c++) {
-            if (gameArray[r][c] === 2048) {
-                youWin()
-                noWinFound = false;
-            }
-        }
-    }
-}
-
-function checkForLoss() {
-    let lossPossible = true;
-    for (r = 0; r < 4 && lossPossible; r++) { // Look at each row
-        for (c = 0; c < 4 && lossPossible; c++) { // Look at each column
-            
-            if (gameArray[r][c] === 0) lossPossible = false; // No loss if any square is empty
-            
-            else if (r > 0) { // No loss if there are matching pieces next to each other
-                if (gameArray[r][c] === gameArray[r - 1][c]) lossPossible = false
-            
-            } else if (c > 0) { // No loss if there are matching pieces above/below each other
-                if (gameArray[r][c] === gameArray[r][c - 1]) lossPossible = false
-            }
-        }
-    }
-    if (lossPossible) youLose()
-}
-
-function youWin() {
-    gameActive = false
-    $('.message').html('You win!<br /> Keep playing as long as you like!')
-    wins++
-    $('.winLossCount').text(`Wins: ${wins} | Losses: ${losses}`)
-}
-
-function youLose() {
-    gameActive = false
-    $('.message').text('You lose!')
-    losses++
-    $('.winLossCount').text(`Wins: ${wins} | Losses: ${losses}`)
-    $('.piece').css('opacity', '0.3')
-}
-
-function keepPlaying() {
-    gameActive = true
-    initialGame = false
-    newPiece()
 }
